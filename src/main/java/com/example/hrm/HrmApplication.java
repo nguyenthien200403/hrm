3a723e2f8c65c7cbf97d8e32eb963f0bba1fdf37
Thien <nguyenngocthien200403@gmail.com>
2025-11-13 13:57:26 +0700

    connect with database sql Server is Success

--- src/main/java/com/example/hrm/HrmApplication.java
+++ src/main/java/com/example/hrm/HrmApplication.java
@@ -1,13 +1,17 @@
 package com.example.hrm;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class HrmApplication {
+public class HrmApplication  {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrmApplication.class, args);
 	}
+
 
 }
