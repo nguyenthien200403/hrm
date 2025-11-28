491aef6d57831f1666dbbaaa2eb0d021bfc1d149
Thien <nguyenngocthien200403@gmail.com>
2025-11-28 08:40:03 +0700

    main

--- src/main/java/com/example/hrm/config/SecurityConfiguration.java
+++ src/main/java/com/example/hrm/config/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import com.example.hrm.security.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
--- src/main/java/com/example/hrm/controller/EmployeeController.java
+++ src/main/java/com/example/hrm/controller/EmployeeController.java
@@ -5,7 +5,6 @@ import com.example.hrm.request.EmployeeRequest;
 import com.example.hrm.service.EmployeeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
