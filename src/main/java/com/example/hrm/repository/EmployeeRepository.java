439d55226556984f493ea4fd804385145f3ad488
Thien <nguyenngocthien200403@gmail.com>
2026-01-10 00:59:27 +0700

    fix

--- src/main/java/com/example/hrm/repository/EmployeeRepository.java
+++ src/main/java/com/example/hrm/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.example.hrm.repository;
 
 import com.example.hrm.model.Employee;
 
+import com.example.hrm.projection.BasicInfoProjection;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -50,6 +51,14 @@ public interface EmployeeRepository extends JpaRepository<Employee, String> {
             "FROM Employee as emp WHERE emp.status = '2'")
     List<EmployeeProjection> findAllProcessing();
 
+
+
+    @Query(value = "EXEC sp_FindAllEmployeeNameByManager :managerId", nativeQuery = true)
+    List<BasicInfoProjection> findAllEmployeeNameByManager(@Param("managerId") String employeeId);
+
+
+    @Query(value = "EXEC sp_FindAllEmployeeNameByAdmin :adminId", nativeQuery = true)
+    List<BasicInfoProjection> findAllEmployeeNameByAdmin(@Param("adminId") String employeeId);
 
 //    @Query("SELECT e.id AS id, " +
 //            "e.name AS name, " +
